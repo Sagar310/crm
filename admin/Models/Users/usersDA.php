225a7b8aa710fbb6068b524c8eb20e666e0ed327
unknown <joshiket@gmail.com>
2018-10-12 12:56:04 +0530

    changes to userDA onverted to Object Oriented

--- admin/Models/Users/usersDA.php
+++ admin/Models/Users/usersDA.php
@@ -3,216 +3,228 @@
     {
         include $class_name.".class.php";
     }
-    $request=json_decode(file_get_contents("php://input"),true);
-    
-    
-    $action=null;
-    if(isset($request["action"]))
-        $action=$request["action"];
-    $msg="";
-   
-    
-    switch($action)
-    {
-            case "newUser":
-                $msg=newUser($request);
-                break;
-            case "userLogin":
-                $msg=userLogin($request);
-                break;
-            case "changePass":
-                $msg=changePass($request);
-                break;
-            case "getResetQ":
-                $msg=getResetQ($request);
-                break;              
-            case "resetPassword":
-                $msg=resetPassword($request);
-                break;    
-            default :
-                $msg="Invalid action.";
-                break;                   
-    }
-    
-    echo $msg;
-    function RANS()
-    {
-        $msgArr["error"]=TRUE;
-        $msgArr["msg"]="Required attribute(s) not set.";
-        return json_encode($msgArr);          
-    }
 
-    function fetchData($request,$var){
-            if(isset($request[$var]))
-                return $request[$var];
+    class RawMaterialDAO{
+        private $request;
+        private $action;
+
+        public function __construct(){
+            $this->request = json_decode(file_get_contents("php://input"),true);
+            $this->action = $this->request["action"];
+        }
+
+        public function RANS()
+        {
+            $msgArr["error"]=TRUE;
+            $msgArr["msg"]="Required attribute(s) not set.";
+            return json_encode($msgArr);          
+        }
+    
+        public function fetchData($var){
+            if(isset($this->request[$var]))
+                return $this->request[$var];
             else
                 return NULL;   
-    }
-
-    function resetPassword($request)
-    {
-        $email = fetchData($request,"email");
-        $resetAns = fetchData($request,"resetAns");
-
-        if($email == NULL || $resetAns == NULL)
-        {
-            $msg = RANS();
         }
-        else
+        public function processAction()
         {
-            $obj = new User();
-            $obj->email = $email;
-            $obj->resetAns = $resetAns;
-            $msg = $obj->resetPassword();
-        }
-        return $msg;
-    }
-
-    function getResetQ($request)
-    {
-        $email = fetchData($request,"email");
-        if($email == NULL)
-        {
-            $msg = RANS();
-        }
-        else
-        {
-            $obj = new User();
-            $obj->email = $email;
-            $msg = $obj->getResetQ();
-        }
-        return $msg;
-    }
-
-    function changePass($request)
-    {
-        $email = fetchData($request,"email");
-        $cpass = fetchData($request,"cpass");
-        $npass = fetchData($request,"npass");
-        if($email == NULL || $cpass == NULL || $npass == NULL)
-        {
-            $msg = RANS();
-        }
-        else
-        {
-            $obj = new User();
-            $obj->email = $email;
-            $obj->cpass = $cpass;
-            $obj->npass = $npass;
-            $msg = $obj->changePassword();
-        }
-        return $msg;
-    }
-
-    function userLogin($request)
-    {
-        $email = fetchData($request,"email");
-        $pass = fetchData($request,"pass");
-        if($email == NULL || $pass == NULL)
-            $msg = RANS();
-        else  
-        {
-            $obj = new User();
-            $obj->email = $email;
-            $obj->pass = $pass;
-            $msg = $obj->userLogin();
-        }
-        return $msg;
-    }
-
-
-    function deleteUser($request)
-    {
-        $msg = "";
-        $mmsgArr = array();
-        $userid = fetchData($request,"userid");
-
-        if($userid == NULL)
-        {
-            RANS();          
-        }
-        else
-        {
-            $obj = new User();   
-            $obj->userid = $userid;               
-            $msg = $obj->deleteUser();
-        }
-        return $msg;        
-    }    
-
-    function updateUser($request)
-    {
-        $msg = "";        
-        $userid =  fetchData($request,"userid");
-        $email = fetchData($request,"email");
-        $pass = fetchData($request,"pass");
-        $resetQ = fetchData($request,"resetQ");
-        $resetAns = fetchData($request,"resetAns");
+            $msg="";   
+    
+       
+            switch($this->action)
+            {
+                        case "newUser":
+                            $msg=$this->newUser();
+                            break;
+                        case "userLogin":
+                            $msg=$this->userLogin();
+                            break;
+                        case "changePass":
+                            $msg=$this->changePass();
+                            break;
+                        case "getResetQ":
+                            $msg=$this->getResetQ();
+                            break;              
+                        case "resetPassword":
+                            $msg=$this->resetPassword();
+                            break;    
+                        default :
+                            $msg="Invalid action.";
+                            break;                   
+            }                  
+            
+            return $msg;            
+        }  
         
-
-        if($userid == NULL || $email == NULL || $pass == NULL || $resetQ == NULL || $resetAns == NULL)
-        { 
-            return RANS();         
-        }
-        else
+        public function resetPassword($request)
         {
-            $obj = new User();   
-            $obj->userid = $userid;
-            $obj->email = $email;         
-            $obj->pass = $pass;         
-            $obj->resetQ = $resetQ;         
-            $obj->resetAns = $resetAns;               
-            $msg = $obj->saveUser();
+            $email = fetchData("email");
+            $resetAns = fetchData("resetAns");
+    
+            if($email == NULL || $resetAns == NULL)
+            {
+                $msg = RANS();
+            }
+            else
+            {
+                $obj = new User();
+                $obj->email = $email;
+                $obj->resetAns = $resetAns;
+                $msg = $obj->resetPassword();
+            }
+            return $msg;
         }
-        return $msg;        
-    }
-        
-    function getUserById($request)
-    {
-        $msg="";
-        $msgArr = array();
-        $userid = fetchData($request,"userid");
-        if($userid==NULL)
+    
+        public function getResetQ($request)
         {
-            RANS();         
+            $email = fetchData("email");
+            if($email == NULL)
+            {
+                $msg = RANS();
+            }
+            else
+            {
+                $obj = new User();
+                $obj->email = $email;
+                $msg = $obj->getResetQ();
+            }
+            return $msg;
         }
-        else
+    
+        public function changePass($request)
         {
-            $obj = new Member();            
-            $msg = $obj->getUser("*","userid=".$userid,"","");
+            $email = fetchData("email");
+            $cpass = fetchData("cpass");
+            $npass = fetchData("npass");
+            if($email == NULL || $cpass == NULL || $npass == NULL)
+            {
+                $msg = RANS();
+            }
+            else
+            {
+                $obj = new User();
+                $obj->email = $email;
+                $obj->cpass = $cpass;
+                $obj->npass = $npass;
+                $msg = $obj->changePassword();
+            }
+            return $msg;
         }
-        return $msg;
-
-    }
-
-    function newUser($request){
-
-        $msg="";
-        $email = fetchData($request,"email");
-        $pass = fetchData($request,"pass");
-        $resetQ = fetchData($request,"resetQ");
-        $resetAns = fetchData($request,"resetAnd");
-        
-        if($email == NULL || $pass  == NULL || $resetQ  == NULL || $resetAns  == NULL)
+    
+        public function userLogin($request)
         {
-            RANS();
+            $email = fetchData("email");
+            $pass = fetchData("pass");
+            if($email == NULL || $pass == NULL)
+                $msg = RANS();
+            else  
+            {
+                $obj = new User();
+                $obj->email = $email;
+                $obj->pass = $pass;
+                $msg = $obj->userLogin();
+            }
+            return $msg;
         }
-        else
+    
+    
+        public function deleteUser($request)
         {
-            $obj=new User();
-            $obj->email =  $email;
-            $obj->pass =  $pass;
-            $obj->resetQ =  $resetAns;
-            $msg=$obj->newUser();
+            $msg = "";
+            $mmsgArr = array();
+            $userid = fetchData("userid");
+    
+            if($userid == NULL)
+            {
+                RANS();          
+            }
+            else
+            {
+                $obj = new User();   
+                $obj->userid = $userid;               
+                $msg = $obj->deleteUser();
+            }
+            return $msg;        
+        }    
+    
+        public function updateUser($request)
+        {
+            $msg = "";        
+            $userid =  fetchData("userid");
+            $email = fetchData("email");
+            $pass = fetchData("pass");
+            $resetQ = fetchData("resetQ");
+            $resetAns = fetchData("resetAns");
+            
+    
+            if($userid == NULL || $email == NULL || $pass == NULL || $resetQ == NULL || $resetAns == NULL)
+            { 
+                return RANS();         
+            }
+            else
+            {
+                $obj = new User();   
+                $obj->userid = $userid;
+                $obj->email = $email;         
+                $obj->pass = $pass;         
+                $obj->resetQ = $resetQ;         
+                $obj->resetAns = $resetAns;               
+                $msg = $obj->saveUser();
+            }
+            return $msg;        
         }
-        return $msg;
-    }
+            
+        public function getUserById()
+        {
+            $msg="";
+            $msgArr = array();
+            $userid = $this->fetchData("userid");
+            if($userid==NULL)
+            {
+                RANS();         
+            }
+            else
+            {
+                $obj = new Member();            
+                $msg = $obj->getUser("*","userid=".$userid,"","");
+            }
+            return $msg;
+    
+        }
+    
+        public function newUser(){
+    
+            $msg="";
+            $email = $this->fetchData("email");
+            $pass = $this->fetchData("pass");
+            $resetQ = $this->fetchData("resetQ");
+            $resetAns = $this->fetchData("resetAnd");
+            
+            if($email == NULL || $pass  == NULL || $resetQ  == NULL || $resetAns  == NULL)
+            {
+                RANS();
+            }
+            else
+            {
+                $obj=new User();
+                $obj->email =  $email;
+                $obj->pass =  $pass;
+                $obj->resetQ =  $resetAns;
+                $msg=$obj->newUser();
+            }
+            return $msg;
+        }
+    
+        public function getAllUsers()
+        {
+            $obj = new User();
+            $msg = $obj->getUser("*","","","");
+            return $msg;    
+        }
+            
+    }//class
 
-    function getAllUsers()
-    {
-        $obj = new User();
-        $msg = $obj->getUser("*","","","");
-        return $msg;    
-    }
-
+    $obj = new UserDAO();
+    $msg = $obj->processAction();
+    echo $msg;    
  
